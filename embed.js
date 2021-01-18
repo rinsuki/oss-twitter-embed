676ae0d0beb2b0a74b55fe4a35bba3ba9c54d22b
rinsuki <428rinsuki+git@gmail.com>
2021-01-18 14:26:40 +0900

    first commit

---
+++ embed.js
@@ -0,0 +1,33 @@
+// https://github.com/rinsuki/oss-twitter-embed
+
+;(() => {
+    var iframes = new Map()
+    var widgetCount = 0
+    window.addEventListener("message", (e) => {
+        if (e.origin !== "https://platform.twitter.com") return
+        const data = e.data["twttr.embed"]
+        if (data == null) return
+        if (data.method !== "twttr.private.resize") return
+        const iframe = iframes.get(data.id)
+        iframe.style.height = `${data.params[0].height}px`
+        iframe.setAttribute("scrolling", "no")
+        console.log(data)
+    })
+    for (const link of document.querySelectorAll(".oss-twitter-embed")) {
+        const url = link.href
+        const tweetIDMatch = /\/status(?:es)?\/([0-9]+)/.exec(url)
+        if (tweetIDMatch == null) continue
+        const tweetID = tweetIDMatch[1]
+        const widgetId = `twitter-widget-oss${++widgetCount}`
+        const iframeURL = `https://platform.twitter.com/embed/index.html?dnt=true&embedId=${widgetId}&id=${tweetID}`
+        const iframe = document.createElement("iframe")
+        iframe.src = iframeURL
+        iframe.style.width = "100%"
+        iframe.style.maxWidth = "550px"
+        iframe.setAttribute("allowtransparency", "1")
+        iframe.setAttribute("allowfullscreen", "1")
+        iframe.setAttribute("frameborder", "0")
+        iframes.set(widgetId, iframe)
+        link.parentElement.replaceChild(iframe, link)
+    }
+})()
